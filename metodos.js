830736a78cd0f62e3dc0380a152e53b75a0e9527
lwazevedo <lwnazevedo@gmail.com>
2017-10-03 23:39:27 -0300

    alteracao nos metodos

---
+++ metodos.js
@@ -0,0 +1,47 @@
+'use strict'
+
+let db = require('./db')
+
+let metodos = {
+  criarUsuario: {
+    descricao: 'Cria um novo usuário',
+    parametros: ['usuario:objeto do usuario'],
+    retorno: ['usuario'],
+    exec(usuarioObj) {
+      return new Promise((resolve) => {
+        if (typeof(usuarioObj) !== 'object') {
+          throw new Error('Um objeto é experado!')
+        }
+        let _usuarioObj = JSON.parse(JSON.stringify(usuarioObj))
+        _usuarioObj.id = (Math.random() * 10000000) | 0
+        resolve(db.usuarios.salvar(_usuarioObj))
+      })
+    }
+  },
+  buscarUsuario: {
+    descricao: 'Buscar usuario pelo id',
+    parametros: ['id:id do usuario que deseja procurar'],
+    retorno: ['usuario'],
+    exec(usuarioObj) {
+      return new Promise((resolve) => {
+        if (typeof(usuarioObj) !== 'object') {
+          throw new Error('Um objeto é experado!')
+        }
+        resolve(db.usuarios.buscar(usuarioObj.id) || {})
+      })
+    }
+  },
+  buscarTodosUsuarios: {
+    liberado: false,
+    descricao: 'Busca lista de usuarios',
+    parametros: [],
+    retorno: [listausuarios],
+    exec() {
+      return new Promise((resolve) => {
+        resolve(db.usuarios.buscarTodos() || {})
+      })
+    }
+  }
+}
+
+module.exports = metodos
